70a74fea95c9964865351be9e437a6bd868a87f3
lambooo1 <logendranl@bzz.ch>
2023-05-05 14:48:30 +0000

    check Coderechtlinien

--- index.js
+++ index.js
@@ -1,160 +1,158 @@
-import express from "express"; 
-import session from "express-session";
+import express from 'express'
+import session from 'express-session'
 
-const app = express(); 
+const app = express()
 const port = 3030
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(session({
   secret: 'supersecret',
-	resave: false,
-	saveUninitialized: true,
+  resave: false,
+  saveUninitialized: true,
   cookie: {}
 }))
 
-//durch ChatGPT generiert
+// durch ChatGPT generiert
 let tasks = [
-    {
-        "id": "1",
-        "Titel": "Einkaufen gehen",
-        "Erstellungsdatum": "2023-05-01",
-        "Erfüllungsdatum": "2023-05-02",
-        "Autor": "max@bsp.com"
-      },
-      {
-        "id": "2",
-        "Titel": "Geburtstagsgeschenk kaufen",
-        "Erstellungsdatum": "2023-04-29",
-        "Erfüllungsdatum": "2023-05-05",
-        "Autor": "max@bsp.com"
-      },
-      {
-        "id": "3",
-        "Titel": "Wohnzimmer streichen",
-        "Erstellungsdatum": "2023-04-30",
-        "Erfüllungsdatum": "2023-05-04",
-        "Autor": "max@bsp.com"
-      },
-      {
-        "id": "4",
-        "Titel": "Buch lesen",
-        "Erstellungsdatum": "2023-05-01",
-        "Erfüllungsdatum": null,
-        "Autor": "max@bsp.com"
-      },
-      {
-        "id": "5",
-        "Titel": "Flug buchen",
-        "Erstellungsdatum": "2023-05-02",
-        "Erfüllungsdatum": null,
-        "Autor": "max@bsp.com"
-      }      
+  {
+    id: '1',
+    Titel: 'Einkaufen gehen',
+    Erstellungsdatum: '2023-05-01',
+    Erfüllungsdatum: '2023-05-02',
+    Autor: 'max@bsp.com'
+  },
+  {
+    id: '2',
+    Titel: 'Geburtstagsgeschenk kaufen',
+    Erstellungsdatum: '2023-04-29',
+    Erfüllungsdatum: '2023-05-05',
+    Autor: 'max@bsp.com'
+  },
+  {
+    id: '3',
+    Titel: 'Wohnzimmer streichen',
+    Erstellungsdatum: '2023-04-30',
+    Erfüllungsdatum: '2023-05-04',
+    Autor: 'max@bsp.com'
+  },
+  {
+    id: '4',
+    Titel: 'Buch lesen',
+    Erstellungsdatum: '2023-05-01',
+    Erfüllungsdatum: null,
+    Autor: 'max@bsp.com'
+  },
+  {
+    id: '5',
+    Titel: 'Flug buchen',
+    Erstellungsdatum: '2023-05-02',
+    Erfüllungsdatum: null,
+    Autor: 'max@bsp.com'
+  }
 ]
 
-
-app.use("/tasks", function (req, res, next) {
-  if (req.session.email == null){
-    res.status(403).json({error: "not authenticated"})
-  }else{
-    next() 
+app.use('/tasks', function (req, res, next) {
+  if (req.session.email == null) {
+    res.status(403).json({ error: 'not authenticated' })
+  } else {
+    next()
   }
 })
 
-app.get("/tasks", (req, res) => { 
+app.get('/tasks', (req, res) => {
   res.status(200).json(tasks)
 })
 
-app.post("/tasks", (req, res) => {
-  if (req.body.Titel == null){
-    res.status(406).json({error: "task must have a title"})
-  }else{
-    let newTask = {}
-  //for-loop inspiriert durch Ben Brändle 
-  for (const value in req.body){
+app.post('/tasks', (req, res) => {
+  if (req.body.Titel == null) {
+    res.status(406).json({ error: 'task must have a title' })
+  } else {
+    const newTask = {}
+    // for-loop inspiriert durch Ben Brändle
+    for (const value in req.body) {
       newTask[value] = req.body[value]
+    }
+    newTask.Autor = req.session.email
+    // mit Lehrer
+    tasks = [...tasks, newTask]
+    res.status(201).json(newTask)
   }
-  newTask.Autor = req.session.email
-  //mit Lehrer
-  tasks = [...tasks, newTask]
-  res.status(201).json(newTask)
-  }  
 })
 
-//eigene Unterlagen
-app.get("/tasks/:id", (req, res) => {
-  const findID = tasks.find(((task) => task.id === req.params.id))
-  //res.json
-  if (findID == null){
-    res.status(404).json({error: "task not found"})
-  }else{
-      res.status(200).json(findID)
+// eigene Unterlagen
+app.get('/tasks/:id', (req, res) => {
+  const findID = tasks.find((task) => task.id === req.params.id)
+  // res.json
+  if (findID == null) {
+    res.status(404).json({ error: 'task not found' })
+  } else {
+    res.status(200).json(findID)
   }
-}) 
+})
 
-app.put("/tasks/:id", (req, res) => {
-  if (req.body.Titel == null){
-    res.status(406).json({error: "task must have a title"})
-  }else{
-    let putTask = {}
-    for (const value in req.body){
+app.put('/tasks/:id', (req, res) => {
+  if (req.body.Titel == null) {
+    res.status(406).json({ error: 'task must have a title' })
+  } else {
+    const putTask = {}
+    for (const value in req.body) {
       putTask[value] = req.body[value]
     }
-    putTask.Autor = req.session.email 
-    //eigene Unterlagen
-    tasks = tasks.map((t) => t.id == putTask.id ? putTask : t)
-    res.json(putTask) 
+    putTask.Autor = req.session.email
+    // eigene Unterlagen
+    tasks = tasks.map((t) => t.id === putTask.id ? putTask : t)
+    res.json(putTask)
   }
 })
 
-app.delete("/tasks/:id", (req, res) => {
-  const deletedTask =  tasks.filter((t) => t.id === req.params.id)
-  tasks = tasks.filter((t) => t.id !== req.params.id); 
-  if (deletedTask == null){
+app.delete('/tasks/:id', (req, res) => {
+  const deletedTask = tasks.filter((t) => t.id === req.params.id)
+  tasks = tasks.filter((t) => t.id !== req.params.id)
+  if (deletedTask == null) {
     res.sendStatus(404)
-  }else{
+  } else {
     res.status(200).json(deletedTask)
   }
 })
 
-const secretAdminCredentials = {email: "beispielmail", password: "m295"}
-//Unterlagen Moodle
-app.post("/login", (req, res) => {
-  const {email, password} = req.body
-  if (password == secretAdminCredentials.password){
-    req.session.email = email 
+const secretAdminCredentials = { email: 'beispielmail', password: 'm295' }
+// Unterlagen Moodle
+app.post('/login', (req, res) => {
+  const { email, password } = req.body
+  if (password === secretAdminCredentials.password) {
+    req.session.email = email
 
-    res.status(200).json({email: req.session.email})
-  }
-  else{
-    res.status(401).json({error: "Invalid credentials"})
-  }
-})
-
-//Unterlagen Moodle
-app.get("/verify", (req, res) => {
-  if (req.session.email){
-    res.status(200).json({email: req.session.email})
-  }else{
-    res.status(401).json({error: "You are not logged in"})
+    res.status(200).json({ email: req.session.email })
+  } else {
+    res.status(401).json({ error: 'Invalid credentials' })
   }
 })
 
-//Unterlagen Moodle
-app.delete("/logout", (req, res) => {
-  if (req.session.email){
+// Unterlagen Moodle
+app.get('/verify', (req, res) => {
+  if (req.session.email) {
+    res.status(200).json({ email: req.session.email })
+  } else {
+    res.status(401).json({ error: 'You are not logged in' })
+  }
+})
+
+// Unterlagen Moodle
+app.delete('/logout', (req, res) => {
+  if (req.session.email) {
     req.session.email = null
     res.status(204).send()
-  }else{
-    res.json({error: "logout not possible"}) 
+  } else {
+    res.json({ error: 'logout not possible' })
   }
 })
 
-//inspiriert durch Ben Brändle 
-app.get("/*", (req, res) => {
-  res.status(404).json({error: "Task not found"})
+// inspiriert durch Ben Brändle
+app.get('/*', (req, res) => {
+  res.status(404).json({ error: 'Task not found' })
 })
 
 app.listen(port, () => {
-    console.log("Server ist gestartet.");
-});
+  console.log('Server ist gestartet.')
+})
